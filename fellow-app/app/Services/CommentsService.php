e39a8198fc9cc26fa420e3c178ababad8e403e46
Lucas Hoffmann <henriquelucashoffmann@gmail.com>
2022-01-15 02:44:41 -0300

    adicionado alguns metodos service comments

--- fellow-app/app/Services/CommentsService.php
+++ fellow-app/app/Services/CommentsService.php
@@ -2,7 +2,33 @@
 
 namespace App\Services;
 
+use App\Http\Controllers\Comments\CommentsController;
+
 class CommentsService
 {
-    
+    public function __construct(CommentsController $commentsRepository)
+    {
+        $this->commentsRepository = $commentsRepository;
+    }
+
+    public function findAllComments()
+    {
+        $comments = $this->commentsRepository->find();
+
+        return $comments;
+    }
+
+    public function findCommentAtPost($idPost)
+    {
+        $postComment = $this->commentsRepository->where("post_id", $idPost)->where("explict", 0)->get();
+
+        return $postComment;
+    }
+
+    public function storeComment(array $data)
+    {   
+        $newComment = $this->commentsRepository($data);
+
+        return $newComment;
+    }
 }
